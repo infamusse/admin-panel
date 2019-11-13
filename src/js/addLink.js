fcbbbd735b5a51fd210e4e4e074a796fe211f03d
mjalowiczor <m.jalowiczor@hotmail.com>
2019-11-13 18:18:14 +0100

    add conections to db

--- src/js/addLink.js
+++ src/js/addLink.js
@@ -3,31 +3,52 @@ const list = linksArticle.querySelector('.section__list');
 const addLinkButton = linksArticle.querySelector('.btn');
 const form = linksArticle.querySelector('.addLink');
 const addButton = form.querySelector('.btn');
-let newLink, linkTitle, link;
-
-/*W pełnej aplikacji form wysyła dane do bazy danych*/
+let newLink, linkTitle;
 
 const createLink = () => {
   form.classList.toggle('addLink--hide');
-  newLink = document.createElement('li');
-  newLink.className = 'section__list-element';
 };
 
-const addLink = newLink => {
-  linkTitle = form.querySelector('input[name="Site name"]').value;
-  link = form.querySelector('textarea[name="link"]').value;
-  console.log('newLink', newLink, linkTitle, link);
-  newLink.innerHTML =
-    `<p>${linkTitle}</p>` +
-    `<a href='http://${link}'>http://${link}</a>` +
-    "<img src='./images/icons/links.svg' alt='' />" +
-    "<img src='./images/icons/trash.svg' alt='' />";
-  list.appendChild(newLink);
-  form.classList.add('addLink--hide');
+const fetchAllLinks = () => {
+  fetch('http://localhost:3131/links')
+    .then(response => response.json())
+    .then(response => displayLinks(response));
+};
+
+const displayLinks = links => {
+  list.innerHTML = '';
+  links.forEach(link => {
+    linkTitle = link.id;
+    link = link.href;
+    newLink = document.createElement('li');
+    newLink.className = 'section__list-element';
+    newLink.innerHTML =
+      `<p>${linkTitle}</p>` +
+      `<a href='http://${link}'>${link}</a>` +
+      '<img src="./images/icons/links.svg" alt="" />' +
+      '<img src="./images/icons/trash.svg" alt="" /';
+    list.appendChild(newLink);
+    form.classList.add('addLink--hide');
+  });
+};
+
+fetchAllLinks();
+
+const sendLink = (id, href) => {
+  fetch('http://localhost:3131/links', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id, href }),
+  }).then(fetchAllLinks);
 };
 
 addLinkButton.addEventListener('click', createLink);
-addButton.addEventListener('click', function(event) {
+
+addButton.addEventListener('click', event => {
   event.preventDefault();
-  addLink(newLink);
+  const linkName = form.querySelector('[name="Site name"]').value;
+  const link = form.querySelector('[name="link"]').value;
+  sendLink(linkName, link);
 });
